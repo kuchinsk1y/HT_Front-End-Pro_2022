bed4dce0d6d95f36079f661ccea442d59fa4a63f
DESKTOP-31QJCO2\shokwave <tim.ku2004@gmail.com>
2022-08-31 14:50:33 +0300

    ht_10

---
+++ dz_10_kuchinskiy/index.js
@@ -0,0 +1,51 @@
+const toDoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < toDoList.length; i++) {
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  toDoList[i].appendChild(span);
+}
+
+const close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    const div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function NewTask() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      const div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
