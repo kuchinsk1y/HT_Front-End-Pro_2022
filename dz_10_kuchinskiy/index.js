f5770ebbfe30e59559ce37b20aacf55d4c746702
DESKTOP-31QJCO2\shokwave <tim.ku2004@gmail.com>
2022-09-02 08:22:25 +0300

    ht_10

--- dz_10_kuchinskiy/index.js
+++ dz_10_kuchinskiy/index.js
@@ -11,25 +11,29 @@ for (i = 0; i < toDoList.length; i++) {
 const close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     const div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-const list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+const list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 function NewTask() {
   const li = document.createElement("li");
   const inputValue = document.getElementById("myInput").value;
   const t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -41,11 +45,13 @@ function NewTask() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+  
+  // Если что по клику на какую-то задачу, она будет зачеркиваться
+  
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       const div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
